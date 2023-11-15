dd408d8dc31ea24edb4bda0aa7a4883592e9416a
ahnjaeyoun <vnm1994wodbs@gmail.com>
2023-11-15 10:45:00 +0900

    2023 11 15 aca

---
+++ boardProject-boot/src/main/java/edu/kh/project/common/config/InterceptorConfig.java
@@ -0,0 +1,36 @@
+package edu.kh.project.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import edu.kh.project.common.interceptor.BoardTypeInterceptor;
+
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer{
+
+	@Bean
+	public BoardTypeInterceptor boardTypeInterceptor() {
+		return new BoardTypeInterceptor();
+	}
+	
+//	@Bean
+//	public BoardTypeInterceptor 다른인터셉터() {
+//		return new BoardTypeInterceptor();
+//	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		
+		registry.addInterceptor(boardTypeInterceptor())
+		.addPathPatterns("/**") // 가로챌 경로 지정(여러개 작성시 , 로 구분)
+		.excludePathPatterns("/css/**", "/images/**", "/js/**"); // 가로채지 않을 경로
+		
+//		registry.addInterceptor(다른인터셉터())
+//		.addPathPatterns("/**") // 가로챌 경로 지정(여러개 작성시 , 로 구분)
+//		.excludePathPatterns("/css/**", "/images/**", "/js/**"); // 가로채지 않을 경로
+	}
+	
+	
+}
